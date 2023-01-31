8743b5ba409b529a7acb62311f8dc158de3842b9
GrazielaRib <grazielaaribeiroo@gmail.com>
2023-01-31 02:28:34 -0300

    Estrutura base do Spring

---
+++ src/test/java/com/soulcodeacademy/MyContacts/MyContactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soulcodeacademy.MyContacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyContactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
